05e917398064b735aba378fe438ad5c8d649ee08
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-01-22 16:09:39 +0530

    feat: set up swagger api in server.js

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,45 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config/app.config';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const PORT = config.port;
 const MONGO_URI = config.mongoURI;
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Project and Task Managemnet System",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Node, Typescript, Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "ProjectManagement",
+        url: "https://projectmanagement.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.ts"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 const startServer = async () => {
   try {
