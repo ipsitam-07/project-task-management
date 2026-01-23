045223ed9d2e80931d2da2787ec06cd21ea74bc9
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-01-23 16:19:42 +0530

    refactor: remove try/catch blocks and implement global error handlers in task controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,42 +1,30 @@
 import { Response } from 'express';
 import { AuthRequest } from '../types';
 import { createTaskService } from '../services/task.service';
+import { AppError } from '../utils/error';
 
 //POST /projects/:projectId/tasks
 export const createTask = async (req: AuthRequest, res: Response) => {
-  try {
     const userId = req.user?.id;
     const projectId = req.params.projectId;
     const { title, description } = req.body;
 
     if (!userId) {
-      return res.status(401).json({
-        success: false,
-        message: 'Unauthorized',
-      });
+        throw new AppError('Unauthorized', 401);
     }
 
     if (!projectId || typeof projectId !== 'string') {
-      return res.status(400).json({
-        success: false,
-        message: 'Invalid project id',
-      });
+        throw new AppError('Inavlid project id', 400);
     }
 
     if (!title) {
-      return res.status(400).json({
-        success: false,
-        message: 'Task title is required',
-      });
+        throw new AppError('Task title is required', 400);
     }
 
     const task = await createTaskService(projectId, userId, { title, description });
 
     if (!task) {
-      return res.status(404).json({
-        success: false,
-        message: 'Project not found',
-      });
+        throw new AppError('Project not found', 404);
     }
 
     return res.status(201).json({
@@ -44,10 +32,4 @@ export const createTask = async (req: AuthRequest, res: Response) => {
       message: 'Task created successfully!',
       data: task,
     });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: 'Failed to create task',
-    });
-  }
 };
